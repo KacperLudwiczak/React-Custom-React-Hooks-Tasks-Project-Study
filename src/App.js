5e8ec37ee723f5e1c4f41a3e01a02489b14cb542
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-10 22:16:35 +0200

    Fixing Loop bug

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
   useEffect(() => {
     fetchTasks();
-  }, [fetchTasks]);
+  }, []);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
